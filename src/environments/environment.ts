eda75f02b867f64f41b78145950ffaf323ad5c76
= <innovatecodes@gmail.com>
2025-11-03 09:21:23 -0300

    Removido arquivos de environments

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-import { IEnvironment } from './environment.d';
-export const environment: IEnvironment = {
-  production: true,
-  viaCepEndpoint: 'https://viacep.com.br/ws/',
-  endpointUri: '/api/post',
-};
